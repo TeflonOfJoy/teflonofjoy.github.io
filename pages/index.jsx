4a8f3f6283c48b0ce81d0b5f76b4e5ff979a1299
TeflonOfJoy <h2sbf7@protonmail.com>
2024-12-17 16:14:23 +0100

    fix picture

--- pages/index.jsx
+++ pages/index.jsx
@@ -75,7 +75,7 @@ export default function HomePage() {
             <div className={styles.picture_boader}>
               <Image
                 className={styles.picture}
-                src="/me.png"
+                src="/me.jpg"
                 width={300}
                 height={300}
                 alt="Spaceman"
