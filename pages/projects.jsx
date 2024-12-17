a30c2f1098553874178c773646de5e546a3afe4c
TeflonOfJoy <h2sbf7@protonmail.com>
2024-12-17 15:20:43 +0100

    remove func

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -7,34 +7,10 @@ const ProjectsPage = ({ ml_projects, bots_projects, pypi_projects, misc_projects
     <>
       <h3>Open Source Projects</h3>
       <br/>
-      <center><h4>Machine Learning</h4></center>
-      <hr/>
-      <div className={styles.container}>
-        {ml_projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
-      <br/>
       <center><h4>Bots</h4></center>
       <hr/>
       <div className={styles.container}>
         {bots_projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
-      <br/>
-      <center><h4>PyPi Packages</h4></center>
-      <hr/>
-      <div className={styles.container}>
-        {pypi_projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
-      <br/>
-      <center><h4>Misc Projects</h4></center>
-      <hr/>
-      <div className={styles.container}>
-        {misc_projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
@@ -43,13 +19,10 @@ const ProjectsPage = ({ ml_projects, bots_projects, pypi_projects, misc_projects
 };
 
 export async function getStaticProps() {
-  const ml_projects = getMLProjects();
   const bots_projects = getBotsProjects();
-  const pypi_projects = getPyPiProjects();
-  const misc_projects = getMiscProjects();
 
   return {
-    props: { title: 'Projects', ml_projects, bots_projects, pypi_projects, misc_projects },
+    props: { title: 'Projects', bots_projects },
   };
 }
 
