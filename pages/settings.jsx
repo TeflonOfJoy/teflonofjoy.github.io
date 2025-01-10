53e14eb600d98650bc0ed16a0ec307cfefd6ec46
TeflonOfJoy <h2sbf7@protonmail.com>
2025-01-10 21:26:07 +0100

    Refactor theme and project image paths; update project details in JSON files
    - Updated image paths for themes in settings.jsx to point to the new directory structure.
    - Changed project image paths in bots-projects.json and misc-projects.json to reflect the new organization.
    - Renamed "ReFurbed" project for consistency.
    - Removed unused image files from the public directory.

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -8,28 +8,28 @@ const SettingsPage = () => {
       <div className={styles.container}>
         <ThemeInfo
           name="Dracula"
-          icon="/dracula.png"
+          icon="/settings/dracula.png"
           publisher="Dracula Theme"
           theme="dracula"
           description="Official Dracula Theme. A dark theme for many editors, shells, and more."
         />
         <ThemeInfo
           name="GitHub Dark"
-          icon="/github-dark.png"
+          icon="/settings/github-dark.png"
           publisher="GitHub"
           theme="github-dark"
           description="GitHub theme for VS Code"
         />
         <ThemeInfo
           name="Catppuccin Mocha"
-          icon="/catppuccin.png"
+          icon="/settings/catppuccin.png"
           publisher="Catppuccin"
           theme="catppuccin-mocha"
           description="Catppuccin is a community-driven pastel theme that aims to be the middle ground between low and high contrast themes."
         />
         <ThemeInfo
           name="Nord"
-          icon="/nord.png"
+          icon="/settings/nord.png"
           publisher="arcticicestudio"
           theme="nord"
           description="An arctic, north-bluish clean and elegant Visual Studio Code theme."
