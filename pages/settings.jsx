6345c40f5167cc1e0c6674170bbab4df70f8ab1c
TeflonOfJoy <h2sbf7@protonmail.com>
2025-01-10 21:29:23 +0100

    revert 53e14eb600d98650bc0ed16a0ec307cfefd6ec46

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -8,28 +8,28 @@ const SettingsPage = () => {
       <div className={styles.container}>
         <ThemeInfo
           name="Dracula"
-          icon="/settings/dracula.png"
+          icon="/dracula.png"
           publisher="Dracula Theme"
           theme="dracula"
           description="Official Dracula Theme. A dark theme for many editors, shells, and more."
         />
         <ThemeInfo
           name="GitHub Dark"
-          icon="/settings/github-dark.png"
+          icon="/github-dark.png"
           publisher="GitHub"
           theme="github-dark"
           description="GitHub theme for VS Code"
         />
         <ThemeInfo
           name="Catppuccin Mocha"
-          icon="/settings/catppuccin.png"
+          icon="/catppuccin.png"
           publisher="Catppuccin"
           theme="catppuccin-mocha"
           description="Catppuccin is a community-driven pastel theme that aims to be the middle ground between low and high contrast themes."
         />
         <ThemeInfo
           name="Nord"
-          icon="/settings/nord.png"
+          icon="/nord.png"
           publisher="arcticicestudio"
           theme="nord"
           description="An arctic, north-bluish clean and elegant Visual Studio Code theme."
