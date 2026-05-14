5f3677cf9a5113da256c1048bb3ed5ed0a215cb3
TeflonOfJoy <h2sbf7@protonmail.com>
2026-05-14 19:59:38 +0200

    removed src and add new cv

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/ResumePage.module.css';
 import { pdfjs, Document, Page } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
-const myResume = './Resume.pdf';
+const myResume = './cv.pdf';
 
 const ResumePage = () => {
   return (
