ab620e077c0b09cdd92d7b053529f6a5a992a070
TeflonOfJoy <h2sbf7@protonmail.com>
2026-05-14 20:11:49 +0200

    change cv name

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/ResumePage.module.css';
 import { pdfjs, Document, Page } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
-const myResume = './cv.pdf';
+const myResume = './CV-NibiziEmanuel.pdf';
 
 const ResumePage = () => {
   return (
